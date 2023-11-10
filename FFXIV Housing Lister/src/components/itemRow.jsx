d7b2677ea96ff2bc87d210b0be14172a91541b47
Melvin <melvinhkusuma@gmail.com>
2023-11-10 20:06:21 +1100

    input quantity validation

--- FFXIV Housing Lister/src/components/itemRow.jsx
+++ FFXIV Housing Lister/src/components/itemRow.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function ItemRow({ item }) {
     const [values, setValues] = useState({
@@ -8,13 +8,20 @@ function ItemRow({ item }) {
         gil: item.gil,
         materials: item.materials
     });
+    const [tempQuantity, setTempQuantity] = useState(1);
+
+    const validateQuantity = (value) => {
+        setValues({ ...values, quantity: (value < 1) ? 1 : (value > 999) ? 999 : value });
+        setTempQuantity((value < 1) ? 1 : (value > 999) ? 999 : value);
+    };
 
     return (
         <tr key={values.id}>
             <td className="pad">{values.name}</td>
             <td className="pad flex">
-                <input type="number" min="1" value={values.quantity}
-                    onChange={e => setValues({ ...values, quantity: e.target.value })}></input>
+                <input type="number" min="1" max="999" value={tempQuantity}
+                    onChange={e => setTempQuantity(e.target.value)}
+                    onBlur={() => validateQuantity(tempQuantity)}></input>
             </td>
             <td className="pad">{values.gil}</td>
             <td className="pad">{values.materials}</td>
