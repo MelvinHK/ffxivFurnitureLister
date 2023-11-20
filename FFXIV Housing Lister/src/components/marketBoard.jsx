9dd2a1c8e40291c1a2afd62beb125e8a8eeb1da8
Melvin <melvinhkusuma@gmail.com>
2023-11-20 15:49:54 +1100

    api call error handling

--- FFXIV Housing Lister/src/components/marketBoard.jsx
+++ FFXIV Housing Lister/src/components/marketBoard.jsx
@@ -20,18 +20,20 @@ function MarketBoard() {
   const handleFetch = async () => {
     if (handleValidation() != "")
       return;
-    
+
     const notGilShopItems = itemList.filter(item => !item.gilShopPrice);
     if (notGilShopItems.length == 0)
       return;
 
     const fetchedListings = await fetchMarketBoardPrices(notGilShopItems.map(item => item.id), location);
-    updateAllMarketBoardPrices(fetchedListings);
+    if (fetchedListings)
+      updateAllMarketBoardPrices(fetchedListings);
   };
 
   return (
     <div>
       <h4>Market Board Prices</h4>
+      <p className="text-small">Fetches market board prices from <a href="https://universalis.app/" target="_blank">Universalis</a>.</p>
       <form className="flex-col w-full border-box">
         <label>Data Centre / Home World </label>
         <select value={location} onChange={e => setLocation(e.target.value)}>
@@ -44,7 +46,7 @@ function MarketBoard() {
           </optgroup>
         </select>
       </form>
-      <button className="pad w-full" onClick={() => { handleFetch(); }}>Fetch</button>
+      <button className="pad w-full" onClick={() => handleFetch()}>Fetch</button>
       <p className="text-small">Items purchased from NPC gil merchants <img src="../../gilShopIcon.webp" className="icon-relative"></img> won't have their prices fetched.</p>
     </div >
   );
--- FFXIV Housing Lister/src/functions.jsx
+++ FFXIV Housing Lister/src/functions.jsx
@@ -9,19 +9,43 @@ export const useOutsideIsClicked = (ref, isOutside) => {
 };
 
 export const fetchItems = async (name) => {
-  const response = await fetch(`https://xivapi.com/search?string=*${name}*&filters=ItemSortCategory.ID=50&limit=10`);
-  const items = await response.json();
-  return items.Results;
+  try {
+    const response = await fetch(`https://xivapi.com/search?string=*${name}*&filters=ItemSortCategory.ID=50&limit=10`);
+    if (!response.ok)
+      throw new Error(response.status);
+
+    const items = await response.json();
+    return items.Results;
+
+  } catch (error) {
+    alert(`Error: Unable to search item; something went wrong with the server request.`);
+  }
 };
 
 export const fetchGilShopPrice = async (id) => {
-  const response = await fetch(`https://xivapi.com/item/${id}`);
-  const item = await response.json();
-  return (item.GameContentLinks.hasOwnProperty("GilShopItem")) ? item.PriceMid : null;
+  try {
+    const response = await fetch(`https://xivapi.com/item/${id}`);
+    if (!response.ok)
+      throw new Error(response.status);
+
+    const item = await response.json();
+    return (item.GameContentLinks.hasOwnProperty("GilShopItem")) ? item.PriceMid : null;
+
+  } catch (error) {
+    alert(`Error: Unable to fetch gil price; something went wrong with the server request.`);
+  }
 };
 
 export const fetchMarketBoardPrices = async (ids, location) => {
-  const response = await fetch(`https://universalis.app/api/v2/${location}/${String(ids)}`);
-  const listings = await response.json();
-  return ids.length > 1 ? listings.items : { [String(ids)]: listings };
+  try {
+    const response = await fetch(`https://universalis.app/api/v2/${location}/${String(ids)}`);
+    if (!response.ok)
+      throw new Error(response.status);
+
+    const listings = await response.json();
+    return ids.length > 1 ? listings.items : { [String(ids)]: listings };
+
+  } catch (error) {
+    alert(`Error: Unable to fetch market board prices; something went wrong with the server request.`);
+  }
 };
