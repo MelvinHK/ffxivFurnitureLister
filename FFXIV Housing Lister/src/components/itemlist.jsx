caba84d317e4687ba34676df3725f98380535ebb
Melvin <melvinhkusuma@gmail.com>
2023-11-28 16:29:03 +1100

    adjusted total materials css, adjusted gil info

--- FFXIV Housing Lister/src/components/itemlist.jsx
+++ FFXIV Housing Lister/src/components/itemlist.jsx
@@ -62,8 +62,8 @@ function ItemList() {
     return (<>
       <h4>Gil Column Info</h4>
       <p className="flex align-center">&#119909;<img className="icon" src="gilShopIcon.webp"></img>&nbsp;= purchased from NPC gil exchange.</p>
-      <p className="flex align-center">&#119909;<img className="icon" src="marketBoardIcon.webp"></img>&nbsp;= purchased from the market board.</p>
       <p className="flex align-center">Empty = not sold by NPC gil exchange; gil price may be fetched from the market board.</p>
+      <p className="flex align-center">&#119909;<img className="icon" src="marketBoardIcon.webp"></img>&nbsp;= purchased from the market board.</p>
       <p className="flex align-center">N/A = no available gil source.</p>
     </>);
   };
