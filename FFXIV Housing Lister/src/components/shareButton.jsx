8818a76784e8f79d54cde9a915a5d36cf3a01ed9
Melvin <melvinhkusuma@gmail.com>
2023-12-06 17:07:47 +1100

    share button modal

--- FFXIV Housing Lister/src/components/shareButton.jsx
+++ FFXIV Housing Lister/src/components/shareButton.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ItemListContext } from "../App";
 
 function ShareButton() {
@@ -10,7 +10,6 @@ function ShareButton() {
     const items = {};
     itemList.content.map(item => items[item.id] = item.quantity);
 
-    console.log(JSON.stringify(items));
     const encodedList = btoa((JSON.stringify(items)));
     const baseURL = window.location;
     const link = `${baseURL}/${encodedList}`;
@@ -27,14 +26,23 @@ function ShareButton() {
       return alert(error);
     }
 
-    handleModal(
-      <>
-        <h4>Share</h4>
-        <div className="sharelink-container">
+    const copyToClipboard = () => navigator.clipboard.writeText(link);
+
+    handleModal(<>
+      <h4>Share List</h4>
+      <p>Shares current state of list.</p>
+      <div className="sharelink-container">
+        <button title="Copy to clipboard" onClick={() => copyToClipboard()} className="flex align-center ml-btn icon-btn">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clipboard" viewBox="0 0 16 16">
+            <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z" />
+            <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z" />
+          </svg>
+        </button>
+        <div className="sharelink pad">
           {link}
         </div>
-      </>
-    );
+      </div>
+    </>);
   };
 
   return (
